73b6aec7756cdf8fe02632a44345a8673e21139c
edumrt <edumoretti28@gmail.com>
2025-10-26 16:02:49 -0300

    inclusao de projeto

---
+++ js/app.js
@@ -0,0 +1,58 @@
+let amigos = [];
+
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo').value;
+
+    if(nomeAmigo == '') {
+        alert('Por favor, insira um nome válido.');
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    
+    let listaAmigos = document.getElementById('lista-amigos');
+    listaAmigos.innerHTML = amigos.join(', ');
+    document.getElementById('nome-amigo').value = '';
+}
+
+function sortear(){
+    if(amigos.length < 3) {
+        alert('Adicione pelo menos 3 amigos para realizar o sorteio!');
+        return;
+    }
+
+    // Embaralhar o array de amigos
+    let embaralhado = amigos.slice().sort(() => Math.random() - 0.5);
+    
+    // Lista para armazenar os pares do sorteio
+    let sorteio = [];
+
+    // Criar os pares de amigo secreto
+    for(let i = 0; i < embaralhado.length; i++) {
+        let pessoa = embaralhado[i];
+        let amigoSecreto;
+        
+        // Se for o último da lista, pega o primeiro
+        if(i === embaralhado.length - 1) {
+            amigoSecreto = embaralhado[0];
+        } else {
+            amigoSecreto = embaralhado[i + 1];
+        }
+        
+        sorteio.push(`${pessoa} -> ${amigoSecreto}`);
+    }
+
+    // Mostrar o resultado
+    document.getElementById('lista-sorteio').innerHTML = sorteio.join('<br>');
+}
+
+function reiniciar(){
+
+    document.getElementById('nome-amigo').value = '';
+    document.getElementById('nome-amigo').value = 'Nome do amigo';
+    document.getElementById('lista-amigos').innerHTML = '';
+    amigos = [];
+
+}
+
+
